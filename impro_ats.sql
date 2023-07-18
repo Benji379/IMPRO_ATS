6f1c48913aaba8954cbf5f06827b9500f25b622f
Benji379 <atochelopezb@gmail.com>
2023-07-18 14:03:36 -0500

    v3

--- impro_ats.sql
+++ impro_ats.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-07-2023 a las 11:02:11
+-- Tiempo de generación: 18-07-2023 a las 21:02:58
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 8.2.0
 
